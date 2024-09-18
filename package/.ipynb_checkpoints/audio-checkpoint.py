2bb39ae62317465fb4538e9db0c57fcc5ecb7f88
kukufuckingkan <keitakukukhan@gmail.com>
2024-09-18 17:19:00 +0000

    audio test

--- package/.ipynb_checkpoints/audio-checkpoint.py
+++ package/.ipynb_checkpoints/audio-checkpoint.py
@@ -1,9 +1,12 @@
 
 from IPython.core.display import HTML
+import pandas as pd
+from IPython.display import HTML
+from package import audio
 
-def player3(filePath: str, env: str) -> str:
+def player(filePath: str, env: str) -> str:
     path = getPath(filePath, env)
-    
+    #print(path)
     # HTML for the audio player without newlines
     audio_html = '''<audio controls style="width:150px">
                       <source src="{}" type="audio/mpeg">
@@ -14,12 +17,18 @@ def player3(filePath: str, env: str) -> str:
 
 
 def getPath(path:str,env: str) -> str:
-    PATH_RESOURCE = '/resource'
+    PATH_RESOURCE = 'resource/'
     
-    if PATH_RESOURCE in path and 'DEV' == env:
-        return path.replace(PATH_RESOURCE,'')
-    return path
+    if PATH_RESOURCE in path:
+        if 'DEV' == env:
+            return path.replace(PATH_RESOURCE,'')
+        return path
+    return ''
 
-    
-    
-    
+
+#resource/asset/audio/1_kan.mp3
+# if __name__ == "__main__":
+#     x = "../resource/asset/audio/1_kan.mp3"
+#     y =x.replace("resource/",'')
+#     print(y)
+#     wavPlayer('kan.mp3')
--- package/audio.py
+++ package/audio.py
@@ -6,7 +6,7 @@ from package import audio
 
 def player(filePath: str, env: str) -> str:
     path = getPath(filePath, env)
-    
+    #print(path)
     # HTML for the audio player without newlines
     audio_html = '''<audio controls style="width:150px">
                       <source src="{}" type="audio/mpeg">
@@ -17,12 +17,18 @@ def player(filePath: str, env: str) -> str:
 
 
 def getPath(path:str,env: str) -> str:
-    PATH_RESOURCE = '/resource'
+    PATH_RESOURCE = 'resource/'
     
-    if PATH_RESOURCE in path and 'DEV' == env:
-        return path.replace(PATH_RESOURCE,'')
-    return path
+    if PATH_RESOURCE in path:
+        if 'DEV' == env:
+            return path.replace(PATH_RESOURCE,'')
+        return path
+    return ''
 
-    
-    
-    
+
+#resource/asset/audio/1_kan.mp3
+# if __name__ == "__main__":
+#     x = "../resource/asset/audio/1_kan.mp3"
+#     y =x.replace("resource/",'')
+#     print(y)
+#     wavPlayer('kan.mp3')
